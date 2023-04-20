fc6a26bf2d06cfba8cd64bae0368db526dc6a57e
Sanyog Singh Rajput <sanyogsingrajput@gmail.com>
2023-04-20 07:52:27 +0530

    new homme

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,155 @@
 import 'package:flutter/material.dart';
-import 'package:practice/Home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MediaQuery(
+        data: MediaQueryData(),
+        child: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // int days = 30;
-
-    return MaterialApp(
-      home: Home()
-       );
+    return Scaffold(
+      backgroundColor: Colors.grey[900], // dark mode background color
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900], // dark mode background color
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: Text(
+          'Its Hairon',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.menu, color: Colors.white),
+                Image.asset(
+                  'assets/logo.png', // replace with your logo asset path
+                  height: 50.0,
+                  width: 50.0,
+                ),
+                Icon(Icons.person, color: Colors.white),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              'Its Hairon',
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'Welcome to Its Hairon',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut elit libero. Curabitur semper suscipit justo, eu convallis risus elementum non.',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      Spacer(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Get Started'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[900], // dark mode
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 12.0,
+        unselectedFontSize: 12.0,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0,
+        onTap: (index) {},
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Bookings',
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Home());
-}
-
 class Home extends StatelessWidget {
-  int day = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hairon"),
       ),
-      body: Material(
-        child: Center(
-          child: Container(
-            child: Text("hll u r F@3k as $day"),
-          ),
-        ),
+      body: Center(
+        child: Text("I'm  Bloody Fooking Peaky Blinders"),
       ),
       drawer: Drawer(),
     );
